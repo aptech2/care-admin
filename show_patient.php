033e12bcc9ed661309b0b029040f0b835ec5081a
aptech2 <danishasif16@gmail.com>
2023-09-08 18:32:53 +0500

    project

---
+++ show_patient.php
@@ -0,0 +1,51 @@
+<?php
+
+include 'header.php';
+?>
+
+   <section id="main-content">
+          <section class="wrapper">
+	<div class="row">
+				
+                <div class="col-md-12">
+                      <div class="content-panel">
+                            <h4><i class="fa fa-angle-right"></i> Basic Table</h4>
+                            <hr>
+                        <table class="table">
+                            <thead>
+                            <tr>
+                                <th>#</th>
+                                <th>First Name</th>
+                                <th>Last Name</th>
+                                <th>Username</th>
+                            </tr>
+                            </thead>
+                            <tbody>
+                            <tr>
+                                <td>1</td>
+                                <td>Mark</td>
+                                <td>Otto</td>
+                                <td>@mdo</td>
+                            </tr>
+                            <tr>
+                                <td>2</td>
+                                <td>Jacob</td>
+                                <td>Thornton</td>
+                                <td>@fat</td>
+                            </tr>
+                            <tr>
+                                <td>3</td>
+                                <td>Larry</td>
+                                <td>the Bird</td>
+                                <td>@twitter</td>
+                            </tr>
+                            </tbody>
+                        </table>
+                      </div><! --/content-panel -->
+                </div><!-- /col-md-12 -->
+</div></section>
+</section>
+<?php
+
+include 'footer.php';
+?>
